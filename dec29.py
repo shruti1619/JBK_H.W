02a3e3f8320ef1ff9b9178ed4bd2dacbcf4f7ac5
shruti1619 <136685394+shruti1619@users.noreply.github.com>
2025-12-30 15:50:07 +0530

    Add files via upload

---
+++ dec29.py
@@ -0,0 +1,24 @@
+# viru=25
+# jay=24
+# gabbar=50
+# if viru>jay and viru>gabbar:
+#     print("viru will pay bill")
+# elif jay>viru and jay>gabbar:
+#     print("jay will pay bill")
+# else:
+#     print("gabbar will pay bill")
+
+# num=int(input("Enter a number: "))
+
+# if ((num%3==0) and (num%5==0)):
+#     print("FIZZ BUZZ")
+    
+# elif (num%5==0):
+#     print("BUZZ")
+# elif (num%3==0):
+#     print("FIZZ")
+# else:
+#     print("number is not divisible by either 3 or 5")
+
+for i in range (len("instagram")):
+    print("instagram"[i])
---
+++ dec30.py
@@ -0,0 +1,32 @@
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(100,9,-1):
+#     print(110-i) #10 to 100 without changing header
+    
+# for i in range(100,9,-1):
+#     print(430-i) #330 to 430
+
+#ICU :- I- initialization
+#       C- condition
+#       U- update
+
+
+# i=0
+# while i<=10:
+#     print(i)
+#     i+=1
+# print(i)    #11
+
+
+# i=0
+# while i<=10:
+#     print(i)
+#     i-=1
+# print(i)    #infinite
+
+for i in range(10):
+    if i%2==1:
+        continue
+    print(i*i)
+    
