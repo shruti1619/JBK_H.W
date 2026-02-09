49999d73c3082ae3cfe497b45581059e4c5a331d
Shruti Morey <136685394+shruti1619@users.noreply.github.com>
2026-02-09 15:45:22 +0530

    Add files via upload

---
+++ Module/feb01.py
@@ -0,0 +1,20 @@
+from math import sqrt,pi,log10,gcd,cbrt,factorial,pow,e,lcm,exp,sin
+import time
+import datetime
+
+start=time.time()
+print(pi)
+print(sqrt(14))
+print(log10(1000))
+print(factorial(5))
+print(gcd(12,15))
+print(cbrt(27))
+print(pow(2,3))
+print(e)
+print(lcm(12,15))
+print(exp(1))
+print(sin(90))
+end=time.time()
+print("Execution Time:", end-start)
+print(time.ctime())
+datetime.time()
